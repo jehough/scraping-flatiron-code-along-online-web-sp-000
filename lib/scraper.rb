15e07421f2237f6ff3fdbf929aa70697b35382ed
Jim Hough <teachlife@outlook.com>
2019-05-07 23:45:07 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -23,6 +23,8 @@ class Scraper
       course.title = post.css('h2').text
       course.schedule = post.css('.date').text
       course.description = post.css('p').text
+    end
+  end
     def print_courses
     self.make_courses
     Course.all.each do |course|
